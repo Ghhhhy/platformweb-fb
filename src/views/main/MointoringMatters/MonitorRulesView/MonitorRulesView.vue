88a3930572bfa3084c5cf6dd5bea720b16455bac
[xiangyi-zhixin] <[email]>
2022-10-13 17:18:56 +0800

    直达资金监控规则库：左侧树选择“区划数”，提示“左侧树加载失败”

--- src/views/main/MointoringMatters/MonitorRulesView/MonitorRulesView.vue
+++ src/views/main/MointoringMatters/MonitorRulesView/MonitorRulesView.vue
@@ -284,7 +284,7 @@ export default {
         multipleValueType: 'String', // 多选值类型 String[逗号分割]，Array //废弃
         treeProps: {
           // 树配置选项
-          labelFormat: '{label}', // {code}-{name}
+          labelFormat: '{code}-{name}', // {code}-{name}
           nodeKey: 'code', // 树的主键
           label: 'name', // 树的显示lalel字段
           children: 'children' // 树的嵌套字段
@@ -841,7 +841,7 @@ export default {
       }
       let that = this
       HttpModule.getLeftTree(params).then(res => {
-        if (res.rscode === '100000') {
+        if (res.code === '000000') {
           let treeResdata = that.getChildrenData(res.data)
           that.treeData = treeResdata
         } else {
