00d1cdd30e8ba27ef644ed6af02ce4b2b4b88955
xuxing <xuxing@bosssoft.com.cn>
2022-12-14 14:11:54 +0800

    perf：导出将$vxeMoney渲染器类型转number

--- src/components/Table/export/export/exportUtil.js
+++ src/components/Table/export/export/exportUtil.js
@@ -103,16 +103,18 @@ export function defaultViewValueFormat(value, row, column) {
       case '$vxeCalculate':
         if (val) {
           val = util.transToNumber(val, props.digits || 2)
-          return util.transToNumberLocalString(val, props.digits || 2)
+          return val * 1
+          // return util.transToNumberLocalString(val, props.digits || 2)
         } else {
-          return ''
+          return 0
         }
       case '$vxeMoney':
         if (val) {
           val = util.transToNumber(val, props.digits || 2)
-          return util.transToNumberLocalString(val, props.digits || 2)
+          return val * 1
+          // return util.transToNumberLocalString(val, props.digits || 2)
         } else {
-          return '0.00'
+          return 0.00
         }
       case '$EditDownConditions':
         return val
