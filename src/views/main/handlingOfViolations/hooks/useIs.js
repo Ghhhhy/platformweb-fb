9f5a42da8240f44787ce3ae041a7ba48ca579855
xuxing <xuxing@bosssoft.com.cn>
2023-03-08 13:51:50 +0800

    fix：违规处理-禁止只对橙色预警有效

--- src/views/main/handlingOfViolations/hooks/useIs.js
+++ src/views/main/handlingOfViolations/hooks/useIs.js
@@ -3,11 +3,12 @@ import { RouterPathEnum, WarnLevelEnum } from '../model/enum'
 
 /**
  * 判断相关hook
- * @param pagePath
- * @param currentNode
+ * @param pagePath 页面路由
+ * @param currentNode 当前选中节点
+ * @param checkedItemsObj 当前勾选列表
  * @return {{isDivisionReAudit: ComputedRef<boolean>, isBlueWarnLevel: ComputedRef<boolean>, isUnitFeedbackPage: ComputedRef<boolean>}}
  */
-export default function useIs(currentNode, pagePath) {
+export default function useIs(currentNode, pagePath, checkedItemsObj) {
   // 是否是处室复审页面（终审）
   const isDivisionReAudit = computed(() => {
     return unref(pagePath) === RouterPathEnum.DIVISION_REAUDIT
@@ -23,9 +24,19 @@ export default function useIs(currentNode, pagePath) {
     return unref(currentNode)?.warnLevel === WarnLevelEnum.BLUE
   })
 
+  // 是否允许禁止操作
+  // 终审 &&（如果是批量操作则判断当前勾选列表中是否存在橙色预警，否判断当前选中处理单是否是橙色预警）
+  const isAllowDisabled = computed(() => {
+    const hasOrangeLevel = unref(checkedItemsObj).length
+      ? unref(checkedItemsObj).some(item => item.warnLevel === WarnLevelEnum.ORANGE)
+      : unref(currentNode).warnLevel === WarnLevelEnum.ORANGE
+    return isDivisionReAudit && hasOrangeLevel
+  })
+
   return {
     isDivisionReAudit,
     isUnitFeedbackPage,
-    isBlueWarnLevel
+    isBlueWarnLevel,
+    isAllowDisabled
   }
 }
