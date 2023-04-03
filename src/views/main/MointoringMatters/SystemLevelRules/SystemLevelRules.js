6b970d50b500bedacbea9b4ce36c5d56c441707a
xuxing <xuxing@bosssoft.com.cn>
2023-04-03 18:22:18 +0800

    style：函数 rename 参照

--- src/views/main/MointoringMatters/SystemLevelRules/SystemLevelRules.js
+++ src/views/main/MointoringMatters/SystemLevelRules/SystemLevelRules.js
@@ -334,7 +334,7 @@ export let proconf = {
           { value: '2', label: '数字' },
           { value: '3', label: '值域' },
           { value: '4', label: '值集' },
-          { value: '5', label: '函数' }
+          { value: '5', label: '参照' }
         ],
         props: {
           placeholder: '关系'
