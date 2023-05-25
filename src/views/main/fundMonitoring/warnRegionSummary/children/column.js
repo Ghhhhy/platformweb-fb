e52d48076ce31c0b9fa92903a570eed99e1c4dc0
LAPTOP-11ITEBNU\liqin <liqin1@bosssoft.com.cn>
2023-05-25 22:13:11 +0800

    支付付款信息

--- src/views/main/fundMonitoring/warnRegionSummary/children/column.js
+++ src/views/main/fundMonitoring/warnRegionSummary/children/column.js
@@ -850,6 +850,38 @@ const proconf = {
       align: 'center'
     },
     {
+      title: '支出功能科目编码',
+      filters: false,
+      width: 140,
+      field: 'zfExpFuncCode',
+      sortable: false,
+      align: 'right'
+    },
+    {
+      title: '支出功能科目名称',
+      filters: false,
+      width: 140,
+      field: 'zfExpFuncName',
+      sortable: false,
+      align: 'right'
+    },
+    {
+      title: '支付方式',
+      filters: false,
+      width: 140,
+      field: 'payTypeName',
+      sortable: false,
+      align: 'right'
+    },
+    {
+      title: '收款人银行',
+      filters: false,
+      width: 140,
+      field: 'payeeAcctBankName',
+      sortable: false,
+      align: 'right'
+    },
+    {
       title: '收款方账号',
       field: 'receiveAccount',
       sortable: false,
@@ -1008,6 +1040,38 @@ const proconf = {
       filters: false,
       sortable: false,
       align: 'center'
+    },
+    {
+      title: '支出功能科目编码',
+      filters: false,
+      width: 140,
+      field: 'zfExpFuncCode',
+      sortable: false,
+      align: 'right'
+    },
+    {
+      title: '支出功能科目名称',
+      filters: false,
+      width: 140,
+      field: 'zfExpFuncName',
+      sortable: false,
+      align: 'right'
+    },
+    {
+      title: '支付方式',
+      filters: false,
+      width: 140,
+      field: 'payTypeName',
+      sortable: false,
+      align: 'right'
+    },
+    {
+      title: '收款人银行',
+      filters: false,
+      width: 140,
+      field: 'payeeAcctBankName',
+      sortable: false,
+      align: 'right'
     },
     {
       title: '收款方账号',
