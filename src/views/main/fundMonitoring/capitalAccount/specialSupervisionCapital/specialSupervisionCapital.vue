f77424c1afdc56f76124731ce90665c1fb5428fa
LAPTOP-11ITEBNU\liqin <liqin1@bosssoft.com.cn>
2023-06-03 23:10:38 +0800

    分资金报错

--- src/views/main/fundMonitoring/capitalAccount/specialSupervisionCapital/specialSupervisionCapital.vue
+++ src/views/main/fundMonitoring/capitalAccount/specialSupervisionCapital/specialSupervisionCapital.vue
@@ -527,7 +527,7 @@ export default {
       if (isInvalidCellValue) return
       let xmSource = 'zdzjxmmx'
       let zcSource = 'zdzjzcmx_fzj'
-      if (this.transJson(this.params5 || '')?.reportCode === 'zxjd_fzj') {
+      if (this.transJson(this.params5 || '')?.reportCode === 'zxjd_fzj' || this.transJson(this.params5 || '')?.reportCode === 'zdzjxmmx') {
         xmSource = 'zxjdxmmx_fzj'
         zcSource = 'zxjdzcmx_fzj'
       }
