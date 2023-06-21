105108eb120ba6e36ea57856dda5d64a7865bdc9
chenxuanke <c18662748057@163.com>
2023-06-21 12:00:52 +0800

    fix:合计行 合计居中问题  预算金额为空时，合计默认为0.00

--- src/views/main/fundMonitoring/capitalAccount/children/column.js
+++ src/views/main/fundMonitoring/capitalAccount/children/column.js
@@ -288,6 +288,7 @@ const proconf = {
           sortable: false,
           width: 140,
           align: 'right',
+          combinedType: 'total',
           cellRender: { name: '$vxeMoney' }
         },
         {
@@ -453,6 +454,7 @@ const proconf = {
       width: 140,
       sortable: true,
       align: 'right',
+      combinedType: 'total',
       cellRender: { name: '$vxeMoney' }
     }
   ],
@@ -727,6 +729,7 @@ const proconf = {
       width: 140,
       sortable: true,
       align: 'right',
+      combinedType: 'total',
       cellRender: { name: '$vxeMoney' }
     }
   ],
@@ -786,6 +789,13 @@ const proconf = {
       align: 'left'
     },
     {
+      title: '转移支付支出功能科目',
+      width: 140,
+      field: 'expFuncName',
+      sortable: false,
+      align: 'left'
+    },
+    {
       title: '分配金额',
       field: 'amountAllfp',
       width: 140,
@@ -807,13 +817,6 @@ const proconf = {
       title: '接收方财政区划',
       width: 140,
       field: 'rec_div_code',
-      sortable: false,
-      align: 'left'
-    },
-    {
-      title: '转移支付支出功能科目',
-      width: 140,
-      field: 'expFuncName',
       sortable: false,
       align: 'left'
     }
@@ -906,6 +909,7 @@ const proconf = {
       width: 140,
       sortable: true,
       align: 'right',
+      combinedType: 'total',
       cellRender: { name: '$vxeMoney' }
     },
     {
@@ -942,6 +946,7 @@ const proconf = {
       width: 140,
       sortable: false,
       align: 'right',
+      combinedType: 'total',
       cellRender: { name: '$vxeMoney' }
     }
   ],
@@ -966,6 +971,7 @@ const proconf = {
       sortable: false,
       width: 140,
       align: 'right',
+      combinedType: 'total',
       cellRender: { name: '$vxeMoney' }
     }
   ],
@@ -1011,6 +1017,7 @@ const proconf = {
       width: 140,
       sortable: true,
       align: 'right',
+      combinedType: 'total',
       cellRender: { name: '$vxeMoney' }
     }
   ],
