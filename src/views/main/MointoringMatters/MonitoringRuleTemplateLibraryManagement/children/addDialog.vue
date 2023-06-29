f6414abf1e11de024ef9bc3f4ccabcc40c0126d7
金江华 <924318103@qq.com>
2023-06-29 18:31:45 +0800

    fix:展示 以及传参修改

--- src/views/main/MointoringMatters/MonitoringRuleTemplateLibraryManagement/children/addDialog.vue
+++ src/views/main/MointoringMatters/MonitoringRuleTemplateLibraryManagement/children/addDialog.vue
@@ -917,6 +917,12 @@ export default {
         businessSystemCodes.push(item.businessSystemCode)
         businessModuleCodes.push(item.businessModuleCode)
       })
+      let replaceStr = ''
+      if (this.fiRuleTypeName.indexOf('中央-') !== -1) {
+        replaceStr = '中央-'
+      } else if (this.fiRuleTypeName.indexOf('地方-') !== -1) {
+        replaceStr = '地方-'
+      }
       if (this.title === '新增') {
         let params = {}
         if (selection.length === 0) {
@@ -930,7 +936,7 @@ export default {
             'businessSystemCode': businessSystemCodes[0],
             'businessModuleCode': businessModuleCodes[0],
             'fiRuleTypeCode': this.fiRuleTypeCode,
-            'fiRuleTypeName': this.fiRuleTypeName
+            'fiRuleTypeName': this.fiRuleTypeName.replace(replaceStr, '')
 
           }
         } else {
@@ -946,7 +952,7 @@ export default {
             'businessModuleCode': businessModuleCodes[0],
             menuName: this.$store.state.curNavModule.name,
             'fiRuleTypeCode': this.fiRuleTypeCode,
-            'fiRuleTypeName': this.fiRuleTypeName
+            'fiRuleTypeName': this.fiRuleTypeName.replace(replaceStr, '')
           }
         }
         HttpModule.addTemplate(params).then(res => {
@@ -968,7 +974,7 @@ export default {
             'ruleAccord': this.ruleAccord,
             'functionCodeList': functionCodes,
             'fiRuleTypeCode': this.fiRuleTypeCode,
-            'fiRuleTypeName': this.fiRuleTypeName
+            'fiRuleTypeName': this.fiRuleTypeName.replace(replaceStr, '')
           }
         } else {
           params = {
@@ -979,7 +985,7 @@ export default {
             'ruleAccord': this.ruleAccord,
             'functionCodeList': functionCodes,
             'fiRuleTypeCode': this.fiRuleTypeCode,
-            'fiRuleTypeName': this.fiRuleTypeName
+            'fiRuleTypeName': this.fiRuleTypeName.replace(replaceStr, '')
           }
         }
         HttpModule.changeTemplate(params).then(res => {
