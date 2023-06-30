85ab69074317667f86bb5fc24890db2983fdc42e
金江华 <924318103@qq.com>
2023-06-30 10:23:53 +0800

    fix:给个最小宽度

--- src/views/main/fundMonitoring/violationHandle/createProcessing/children/addDialog.vue
+++ src/views/main/fundMonitoring/violationHandle/createProcessing/children/addDialog.vue
@@ -1296,7 +1296,9 @@ export default {
 </script>
 <style lang="scss">
   .payVoucherInput {
-    min-width: 1440px;
+    .el-main{
+      min-width: 350px;
+    }
     margin: 15px;
     .el-card {
       margin-top: 0
