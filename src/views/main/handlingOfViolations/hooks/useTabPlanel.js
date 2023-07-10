148d11a935aa2d23c337e95ed13764570153cf42
金江华 <924318103@qq.com>
2023-07-10 18:14:12 +0800

    fix:撤回逻辑

--- src/views/main/handlingOfViolations/hooks/useTabPlanel.js
+++ src/views/main/handlingOfViolations/hooks/useTabPlanel.js
@@ -2,7 +2,7 @@ import { ref, unref } from '@vue/composition-api'
 import { Message, MessageBox } from 'element-ui'
 import { ModalTypeEnum, RouterPathEnum, TabEnum } from '../model/enum'
 import { allTab, returnTab, disabledTab, sendAuditTabs, doAuditTabs } from '../model/data'
-import { billInvalid } from '@/api/frame/main/handlingOfViolations/index.js'
+import { billInvalid, workFlowRevoke } from '@/api/frame/main/handlingOfViolations/index.js'
 import { checkRscode } from '@/utils/checkRscode'
 import store from '@/store'
 
@@ -79,6 +79,30 @@ function useTabPlanel(
       }
     })
   }
+  async function revoke() {
+    const tableInstance = await getTable()
+    checkedRecords.value = tableInstance.getCheckboxRecords(true)
+    if (!unref(checkedRecords)?.length) {
+      Message.warning('请勾选需要撤回的数据')
+      return
+    }
+    MessageBox.confirm('确认撤回所选数据吗？', '温馨提示', {
+      type: 'warning',
+      callback: async action => {
+        if (action === 'confirm') {
+          const menuId = store.state.curNavModule?.guid || ''
+          const params = {
+            menuId,
+            status: '3', // 撤回
+            data: unref(checkedRecords).map(item => { return { id: item.id, warnLevel: item.warnLevel } })
+          }
+          checkRscode(await workFlowRevoke(params))
+          Message.success(`成功撤回${unref(checkedRecords)?.length}条数据`)
+          resetFetchTableData()
+        }
+      }
+    })
+  }
 
   // 违规处理按钮
   const auditButton = {
@@ -96,12 +120,20 @@ function useTabPlanel(
     label: '作废',
     callback: () => cancellationHandle()
   }
+  // 撤回
+  const revokeButton = {
+    status: 'primary',
+    label: '撤回',
+    callback: () => revoke()
+  }
   // 最终按钮配置
   const tabMapButton = Object.values(TabEnum).reduce((map, value) => {
     if ([TabEnum.NO_AUDIT, TabEnum.NO_SEND].includes(value)) {
       Reflect.set(map, value, [auditButton])
     } else if ([TabEnum.RETURN].includes(value)) {
       Reflect.set(map, value, [auditButton, previewButton])
+    } else if ([TabEnum.SENDED].includes(value)) {
+      Reflect.set(map, value, [revokeButton, previewButton])
     } else {
       Reflect.set(map, value, [previewButton])
     }
