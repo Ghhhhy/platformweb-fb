4e9b681049e884f6e4a4fdc776e9eada34baaf0b
LAPTOP-11ITEBNU\liqin <liqin1@bosssoft.com.cn>
2023-07-21 11:50:29 +0800

    批量认定接口

--- src/views/main/fundMonitoring/departWarningResultHandleRule/children/wdetailDialog.vue
+++ src/views/main/fundMonitoring/departWarningResultHandleRule/children/wdetailDialog.vue
@@ -57,6 +57,7 @@
       v-if="affirmDialogVisibles"
       :title="dialogTitle1"
       :select-data="selectData"
+      :select-ids="selectIds"
       @close="closeHandle"
     />
     <GlAttachment
@@ -120,6 +121,7 @@ export default {
     return {
       userInfo: {},
       billguid: '',
+      selectIds: [],
       showGlAttachmentDialog: false,
       tabStatusBtnConfig: {
         // changeBtns: true,
