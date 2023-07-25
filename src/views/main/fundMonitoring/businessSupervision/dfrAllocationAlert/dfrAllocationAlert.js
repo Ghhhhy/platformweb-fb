73214361af485992706634e689a3e85c714c0b75
金江华 <924318103@qq.com>
2023-07-25 17:14:57 +0800

    fix:直达资金分配预警表代码合并

--- src/views/main/fundMonitoring/businessSupervision/dfrAllocationAlert/dfrAllocationAlert.js
+++ src/views/main/fundMonitoring/businessSupervision/dfrAllocationAlert/dfrAllocationAlert.js
@@ -1,4 +1,4 @@
-// import store from '@/store/index'
+import store from '@/store/index'
 const proconf = {
   // BsToolBar 状态栏
   toolBarStatusButtons: [
@@ -47,14 +47,16 @@ const proconf = {
     }
   ],
   highQueryData: {
-    fiscalYear: ''
+    fiscalYear: store.state?.userInfo?.year
   },
   basicInfo: {
     type: 'form',
     tableConfig: {
       globalConfig: {
         // 全局配置
-        seq: true // 序号列
+        seq: true, // 序号列
+        useMoneyFilter: true,
+        checkType: 'checkbox'
       }
     },
     tableColumnsConfig: [
@@ -73,7 +75,7 @@ const proconf = {
         title: '专项资金目录编码',
         field: 'speTypeCode',
         width: 120,
-        sortable: false,
+        sortable: true,
         filters: false,
         align: 'center'
       },
@@ -81,7 +83,7 @@ const proconf = {
         title: '专项资金目录名称',
         field: 'speTypeName',
         width: 120,
-        sortable: false,
+        sortable: true,
         filters: false,
         align: 'center'
       },
@@ -105,65 +107,63 @@ const proconf = {
         title: '指标文号',
         field: 'corBgtDocNo',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '项目编码',
         field: 'proCode',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '项目名称',
         field: 'proName',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '下达金额',
         field: 'amount',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center',
+        combinedType: [
+          'average',
+          'subTotal',
+          'total',
+          'totalAll'
+        ],
         cellRender: { name: '$vxeMoney' }
       },
       {
         title: '下达时间',
         field: 'xdTime',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '登记时间',
         field: 'djTime',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '登记天数',
         field: 'djDate',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '未登记预警',
         field: 'wdjWarn',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center',
         name: '$vxeSelect',
         cellRender: {
@@ -180,33 +180,35 @@ const proconf = {
         title: '分配金额',
         field: 'fpAmount',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center',
+        combinedType: [
+          'average',
+          'subTotal',
+          'total',
+          'totalAll'
+        ],
         cellRender: { name: '$vxeMoney' }
       },
       {
         title: '分配时间',
         field: 'fpTime',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '分配天数',
         field: 'fpDate',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '已登记未分配预警',
         field: 'ydjwfpWarn',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center',
         name: '$vxeSelect',
         cellRender: {
@@ -223,26 +225,37 @@ const proconf = {
         title: '追加金额',
         field: '',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center',
         children: [
           {
             title: '本级',
             field: 'zjBjamount',
             width: 120,
-            sortable: false,
+            sortable: true,
             filters: false,
             align: 'center',
+            combinedType: [
+              'average',
+              'subTotal',
+              'total',
+              'totalAll'
+            ],
             cellRender: { name: '$vxeMoney' }
           },
           {
             title: '下级',
             field: 'zjXjamount',
             width: 120,
-            sortable: false,
-            filters: false,
+            sortable: true,
+            filters: true,
             align: 'center',
+            combinedType: [
+              'average',
+              'subTotal',
+              'total',
+              'totalAll'
+            ],
             cellRender: { name: '$vxeMoney' }
           }
         ]
@@ -251,24 +264,21 @@ const proconf = {
         title: '追加时间',
         field: 'zjTime',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '已分配追加天数',
         field: 'yfpzjDate',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '已分配未追加预警',
         field: 'yfpwzjWarn',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center',
         name: '$vxeSelect',
         cellRender: {
@@ -285,56 +295,49 @@ const proconf = {
         title: '指标来源代码',
         field: 'sourceTypeCode',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '指标来源名称',
         field: 'sourceTypeName',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '资金来源代码',
         field: 'foundTypeCode',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '资金来源名称',
         field: 'foundTypeName',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '资金性质代码',
         field: 'fundTypeCode',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       {
         title: '资金性质名称',
         field: 'fundTypeName',
         width: 120,
-        sortable: false,
-        filters: false,
+        sortable: true,
         align: 'center'
       },
       // {
       //   title: '支出功能科目编码',
       //   field: 'expFuncCode',
       //   width: 120,
-      //   sortable: false,
-      //   filters: false,
+      //   sortable: true,
       //   align: 'center'
       // },
       {
@@ -371,36 +374,48 @@ const proconf = {
         align: 'right'
       },
       {
-        title: '指标管理处室代码',
-        field: 'bgtMofDepCode',
-        width: 120,
-        sortable: false,
-        filters: false,
-        align: 'center'
-      },
-      {
-        title: '指标管理处室名称',
+        title: '指标管理处室',
         field: 'bgtMofDepName',
         width: 120,
-        sortable: false,
-        filters: false,
-        align: 'center'
+        sortable: true,
+        align: 'center',
+        cellRender: {
+          name: '$vxeInput',
+          options: [],
+          defaultValue: '',
+          props: {
+            format: '{bgtMofDepCode}-{bgtMofDepName}'
+          }
+        },
+        props: {
+          format: '{bgtMofDepCode}-{bgtMofDepName}'
+        }
       },
+      // {
+      //   title: '业务主管处室代码',
+      //   field: 'manageMofDepCode',
+      //   width: 120,
+      //   sortable: false,
+      //   filters: false,
+      //   align: 'center'
+      // },
       {
-        title: '业务主管处室代码',
-        field: 'manageMofDepCode',
-        width: 120,
-        sortable: false,
-        filters: false,
-        align: 'center'
-      },
-      {
-        title: '业务主管处室名称',
+        title: '业务主管处室',
         field: 'manageMofDepName',
         width: 120,
-        sortable: false,
-        filters: false,
-        align: 'center'
+        sortable: true,
+        align: 'center',
+        cellRender: {
+          name: '$vxeInput',
+          options: [],
+          defaultValue: '',
+          props: {
+            format: '{manageMofDepCode}-{manageMofDepName}'
+          }
+        },
+        props: {
+          format: '{manageMofDepCode}-{manageMofDepName}'
+        }
       }
     ],
     tableData: []
