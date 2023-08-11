55a195f0b38dfcf65fc486bffd709de2db9101c6
金江华 <924318103@qq.com>
2023-08-11 17:54:40 +0800

    20230811-重庆-fix-方法未定义报错卡住请求table数据

--- src/api/frame/main/fundMonitoring/notFillBenefitDetail.js
+++ src/api/frame/main/fundMonitoring/notFillBenefitDetail.js
@@ -3,5 +3,8 @@ export default {
   // 获取表格数据
   queryTableDatas (params) {
     return post('dfr-monitor-service/dfr/notFillBenefitDetail/pageQuery', params)
+  },
+  getProTreeData(params) {
+    return post('dfr-monitor-service/dfr/mofDivList/queryPro', params)
   }
 }
