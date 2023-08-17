019f1e0d0bb7840eac7904d97c2b833b116f0699
金江华 <924318103@qq.com>
2023-08-17 21:20:04 +0800

    fix:金额元/万元切换

--- src/views/main/fundMonitoring/capitalCarryForward/carryPlm/carryPlm.vue
+++ src/views/main/fundMonitoring/capitalCarryForward/carryPlm/carryPlm.vue
@@ -47,7 +47,7 @@
           <template v-slot:toolbarSlots>
             <div class="table-toolbar-left">
               <div class="table-toolbar-left-title">
-                <span class="fn-inline">{{ menuName }}</span>
+                <span class="fn-inline">直达机制项目全生命周期管理情况表(单位:{{ moneyUnit }})</span>
                 <i class="fn-inline"></i>
               </div>
             </div>
@@ -72,6 +72,13 @@ export default {
     },
     queryConfig() {
       this.getSearchDataList()
+    }
+  },
+  computed: {
+    moneyUnit() {
+      const moneyUnitValue = this.$refs.bsTableRef.moneyUnit || 10000
+      const moneyUnitMap = { 10000: '万元', 1: '元' }
+      return moneyUnitMap[moneyUnitValue]
     }
   },
   data() {
