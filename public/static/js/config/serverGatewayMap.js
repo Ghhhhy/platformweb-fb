97f0918dc422295defa72dfd191ae1d7885355e0
金江华 <924318103@qq.com>
2023-08-24 12:05:30 +0800

    fix: 网关提交还原

--- public/static/js/config/serverGatewayMap.js
+++ public/static/js/config/serverGatewayMap.js
@@ -1,7 +1,4 @@
-// const api = 'http://223.223.190.114:12218'
-// const api = 'http://223.223.190.114:21105'
-const api = 'http://10.100.32.125:7914'
-// const api = 'http://43.143.57.251:6007'
+const api = 'http://223.223.190.114:12218'
 
 // 帆软报表服务地址
 let reportService = 'http://10.100.59.193:48000/boss/ReportServer?reportlet='
@@ -15,17 +12,9 @@ window.gloableToolFn = {
       'mp-b-perm-service': '/apaas/api/mp-b-perm-service/',
       'mp-b-basedata-service': '/apaas/api/mp-b-basedata-service/',
       'mp-d-aggregation-service': '/apaas/api/mp-d-aggregation-service',
-      // --------------------------- apass服务 sh---------------------------------//
-      // 'mp-b-sso-service': '/api/mp-b-sso-service/',
-      // 'mp-b-user-service': '/api/mp-b-user-service',
-      // 'mp-b-perm-service': '/api/mp-b-perm-service/',
-      // 'mp-b-basedata-service': '/api/mp-b-basedata-service/',
-      // 'mp-d-aggregation-service': '/api/mp-d-aggregation-service',
-      // // --------------------------- 文件相关服务 --------------------------------//
-      fileservice: '/api/fileservice/',
-      filePreviewService: '/api/filePreviewService/',
-      // fileservice: '/apaas/api/fileservice/',
-      // filePreviewService: '/apaas/api/filePreviewService/',
+      // --------------------------- 文件相关服务 --------------------------------//
+      fileservice: '/apaas/api/fileservice/',
+      filePreviewService: '/apaas/api/filePreviewService/',
       // --------------------------- 后台服务 -----------------------------------//
       'large-monitor-platform': '',
       'dfr-monitor-service': ''
@@ -37,38 +26,20 @@ window.gloableToolFn = {
       'mp-b-perm-service': api + '/apaas/api/mp-b-perm-service/',
       'mp-b-basedata-service': api + '/apaas/api/mp-b-basedata-service/',
       'mp-d-aggregation-service': api + '/apaas/api/mp-d-aggregation-service',
-      // fileservice: api + '/apaas/api/fileservice/',
-      // --------------------------- apass  sh 服务 ---------------------------------//
-      // 'mp-b-sso-service': api + '/mp-b-sso-service/',
-      // 'mp-b-user-service': api + '/mp-b-user-service/',
-      // 'userservice': api + '/userservice/',
-      // 'mp-b-perm-service': api + '/mp-b-perm-service/',
-      // 'mp-b-basedata-service': api + '/mp-b-basedata-service/',
-      // 'mp-d-aggregation-service': api + '/mp-d-aggregation-service',
       // --------------------------- 文件服务 ----------------------------------//
-      fileservice: api + '/openapi/fileservice/',
+      fileservice: api + '/apaas/api/fileservice/',
       filePreviewService: api + '/filePreviewService',
       // --------------------------- 后台服务 ----------------------------------//
-      // // 'large-monitor-platform': 'http://192.168.0.113:6602',
-      // 'large-monitor-platform': api,
-      // 'dfr-monitor-service': api,
-      'budgetServer': 'http://10.100.32.125:9008/',
-
-      // 'large-monitor-platform': api,
-      'large-monitor-platform': 'http://127.0.0.1:6602',
-      // 'dfr-monitor-service': api
-      // 'dfr-monitor-service': 'http://192.168.0.230:6603'
-      'dfr-monitor-service': 'http://127.0.0.1:6603'
+      // 'large-monitor-platform': 'http://192.168.0.113:6602',
+      'large-monitor-platform': api,
+      'dfr-monitor-service': api,
+      'budgetServer': 'http://10.100.32.125:9008/'
     },
     gloableUrl: {
       // 是否退出到门户（平台）
       isLoginOutToPortal: true,
       // 门户（平台）登录地址
-      // portalLoginUrl: 'http://223.223.190.114:12100/'
-      // 上海
-      // portalLoginUrl: 'http://43.143.57.251:6007/'
-      // portalLoginUrl: 'http://223.223.190.114:21105/'
-      portalLoginUrl: 'http://10.100.32.125:9007/'
+      portalLoginUrl: 'http://223.223.190.114:12100/'
     }
   },
   // 是否开启水印
