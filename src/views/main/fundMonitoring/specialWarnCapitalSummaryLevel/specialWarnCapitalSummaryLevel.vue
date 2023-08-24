85e308f056fa8c75db0885895d8b9ecde02c0956
金江华 <924318103@qq.com>
2023-08-24 16:50:36 +0800

    fix: 导出金额转换失效的问题

--- src/views/main/fundMonitoring/specialWarnCapitalSummaryLevel/specialWarnCapitalSummaryLevel.vue
+++ src/views/main/fundMonitoring/specialWarnCapitalSummaryLevel/specialWarnCapitalSummaryLevel.vue
@@ -40,6 +40,7 @@
           @cellDblclick="cellDblclick"
           @cellClick="cellClick"
           @onToolbarBtnClick="onToolbarBtnClick"
+          @switchMoneyUnit="switchMoneyUnit"
         >
           <template v-slot:toolbarSlots>
             <div class="table-toolbar-left">
@@ -95,10 +96,8 @@ export default {
       let dataType = this.transJson(this.$store.state.curNavModule.param5 || '').exportModalDefaultSelect || 'fullData'
       return {
         customExportConfig: {
-          dataType: dataType,
-          addUnitColumn: true,
-          addReportTitleColumn: true,
-          fileName: this.menuName
+          ...this.tableGlobalConfig.customExportConfig,
+          dataType
         }
       }
     }
@@ -133,6 +132,13 @@ export default {
       tableColumnsConfig: getFormData('basicInfo', 'tableColumnsConfig'),
       tableData: [],
       obj: {},
+      tableGlobalConfig: {
+        customExportConfig: {
+          addUnitColumn: true,
+          addReportTitleColumn: true,
+          unit: '万元'
+        }
+      },
       calculateConstraintConfig: {
         enabled: true,
         extendMapInfoField: true, // 是否扩展mapInfo字段
@@ -246,6 +252,9 @@ export default {
     // this.getNewData()
   },
   methods: {
+    switchMoneyUnit(level) {
+      this.tableGlobalConfig.customExportConfig.unit = level === 1 ? '元' : '万元'
+    },
     // 展开折叠查询框
     onQueryConditionsClick(isOpen) {
       this.isShowQueryConditions = isOpen
