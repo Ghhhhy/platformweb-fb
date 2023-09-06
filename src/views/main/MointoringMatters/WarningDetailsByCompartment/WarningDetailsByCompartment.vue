6b3b65f13fa2032cfdb9091947320174738f2f80
金江华 <924318103@qq.com>
2023-09-06 22:10:05 +0800

    fix:传参错误

--- src/views/main/MointoringMatters/WarningDetailsByCompartment/WarningDetailsByCompartment.vue
+++ src/views/main/MointoringMatters/WarningDetailsByCompartment/WarningDetailsByCompartment.vue
@@ -474,7 +474,7 @@ export default {
           }
           this.dialogTitle = '详细信息'
           this.warningCode = this.selectData.warningCode
-          this.fiRuleCode = this.selectData.firulecode
+          this.fiRuleCode = this.selectData.fiRuleCode
           break
         case 'violation':
           let selectionData = this.$refs.mainTableRef.getSelectionData()
