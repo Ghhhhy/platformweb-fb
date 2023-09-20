71902cd37e0ffe01af5685130fdccc3e4c41c266
金江华 <924318103@qq.com>
2023-09-20 22:42:23 +0800

    fix: 字段取值问题

--- src/views/main/MointoringMatters/StatisticalFormsByRank/StatisticalFormsByRank.js
+++ src/views/main/MointoringMatters/StatisticalFormsByRank/StatisticalFormsByRank.js
@@ -329,7 +329,7 @@ export let proconf = {
         },
         {
           title: '涉及金额',
-          field: 'orderCorrectionAmount ',
+          field: 'correctedAmount ',
           width: '150',
           align: 'center'
         },
