a9f05f462a07bdac79d1cf78ce86abf5628fb523
金江华 <924318103@qq.com>
2023-10-07 10:52:28 +0800

    feat: “保工资”预警表开发

---
+++ src/views/main/MointoringMatters/guaranteedSalaryWarningTable/guaranteedSalaryWarningTableData.js
@@ -0,0 +1,178 @@
+/*eslint-disable*/
+import store from '@/store/index'
+import Vue from 'vue'
+const h = new Vue().$createElement
+export const mockQueryData = [
+  {
+    title: '日期',
+    field: 'startDays',
+    sortable: false,
+    align: 'left',
+    itemRender: {
+      name: '$vxeTime',
+      defaultValue: '',
+      format: 'YYYY-MM-DD',
+      props: {
+        type: 'date', //
+        placeholder: '请选择日期'
+      }
+    }
+  },
+  {
+    title: '区划',
+    field: 'agencyCode',
+    align: 'left',
+    name: '$vxeTree',
+    itemRender: {
+      name: '$vxeTree',
+      options: [],
+      defaultValue: '',
+      props: {
+        placeholder: '请选择区划',
+        isServer: true,
+        config: {
+          treeProps: {
+            nodeKey: 'guid',
+            label: 'name',
+            children: 'children'
+          },
+          placeholder: '请选择区划',
+          multiple: true,
+          isleaf: false,
+          axiosConfig: {
+            method: 'post',
+            successCode: '100000',
+            statusField: 'rscode',
+            url: 'dfr-monitor-service/dfr/mofDivList/query'
+          }
+        },
+        queryparams: {
+          fiscalYear: '2023'
+        }
+      }
+    }
+  },
+  {
+    title: '预警级别',
+    field: 'warningLevel',
+    align: 'center',
+    itemRender: {
+      name: '$vxeSelect',
+      options:[],
+      options: store.state.warnInfo.warnLevelOptions.map(item => {
+        return {
+          ...item,
+          value: String(item.value)
+        }
+      }),
+      defaultValue: '',
+      props: {}
+    },
+    sortable: 'true',
+    name: '$vxeSelect'
+  }
+]
+export const mockQueryParams = {
+  warningLevel: '',
+  agencyCode: '',
+  startDays: ''
+}
+export const mockTableColumns = [
+  // {
+  //   title: '区划',
+  //   width: '180',
+  //   field: 'mofDivName',
+  //   align: 'left',
+  //   type: 'treeNode',
+  //   treeNode: true
+  // },
+  {
+    title: '区划',
+    align: 'left',
+    treeNode: true,
+    width: 260,
+    field: 'mofDivName',
+    filters: false,
+    cellRender: {
+      name: '$vxeIcon'
+    }
+  },
+  {
+    title: '预算数',
+    field: 'sbZbjeBgz',
+    width: 200,
+    align: 'right',
+    combinedType: ['average', 'subTotal', 'total', 'totalAll'],
+    filters: false,
+    cellRender: {
+      name: '$vxeMoney'
+    },
+    // formula: '{sbZbjeBjbms}+{sbZbjeBgz}+{sbZbjeByz}'
+  },
+  {
+    title: '执行数',
+    field: 'sbZxjeBgz',
+    width: 200,
+    align: 'right',
+    combinedType: ['average', 'subTotal', 'total', 'totalAll'],
+    filters: false,
+    cellRender: {
+      name: '$vxeMoney'
+    },
+    // formula: '{sbZbjeBjbms}+{sbZbjeBgz}+{sbZbjeByz}'
+  },
+  {
+    title: '考核进度',
+    field: 'sbGzKhjd',
+    width: 200,
+    align: 'right',
+    combinedType: ['average', 'subTotal', 'total', 'totalAll'],
+    filters: false,
+    cellRender: {
+      name: '$vxeMoney'
+    },
+    // formula: '{sbZbjeBjbms}+{sbZbjeBgz}+{sbZbjeByz}'
+  },
+  {
+    title: '序时进度',
+    field: 'XSJD',
+    width: 200,
+    align: 'right',
+    combinedType: ['average', 'subTotal', 'total', 'totalAll'],
+    filters: false,
+    cellRender: {
+      name: '$vxeMoney'
+    },
+    // formula: '{sbZbjeBjbms}+{sbZbjeBgz}+{sbZbjeByz}'
+  },
+  {
+    title: '预警级别',
+    field: 'warningLevel',
+    width: 200,
+    align: 'center',
+    filters: false,
+    cellRender: {
+      name: '$vxeInput'
+    },
+    formatter({ row }) {
+      return row.warningLevel
+    },
+    slots: {
+      default({ row }) {
+        return [
+          <div style={{ backgroundColor: row.color }}>{row.warningLevel}</div>
+        ]
+      }
+    },
+    exportFormatter: true,
+    customerExportStyle: ({ row }) => {
+      return {
+        fill: {
+          bgColor: 'ffffffff',
+          fgColor: `ff${row.color?.replace('#', '')}`,
+          patternType: 'solid'
+        }
+      }
+    }
+  }
+]
