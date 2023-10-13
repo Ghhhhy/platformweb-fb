6f046e4e0d1c0a5106a75af7508347b49e33b52e
金江华 <924318103@qq.com>
2023-10-13 13:19:30 +0800

    feat：新增水平移动指令

--- src/directive/customerDirective.js
+++ src/directive/customerDirective.js
@@ -56,5 +56,64 @@ export default {
         })
       }
     })
+    // 水平拖动指令
+    Vue.directive('xResize', {
+      inserted: (el, binding) => {
+        // 添加拖动dom
+        el.style.position = 'relative'
+        const dargNodeDom = document.createElement('div')
+        dargNodeDom.style.position = 'absolute'
+        dargNodeDom.style.right = '-7px'
+        dargNodeDom.style.bottom = '0'
+        dargNodeDom.style.width = '14px'
+        dargNodeDom.style.height = '100%'
+        dargNodeDom.style['text-align'] = 'center'
+        dargNodeDom.style.cursor = 'ew-resize'
+        el.appendChild(dargNodeDom)
+        // 拿到传参的值 默认最大宽度为父元素盒子宽度 （这儿建议给父元素设置display：flex,其余子盒子使用剩余宽度）
+        const { max = el.parentNode.offsetWidth, min = 100 } = binding.value || {}
+        const curDomStyle = {
+          min: min,
+          max: max,
+          _width: el.offsetWidth
+        }
+        // 设置边界条件
+        Object.defineProperty(curDomStyle, 'width', {
+          set(newValue) {
+            if (newValue > curDomStyle.max) { // curDomStyle.max
+              this._width = curDomStyle.max
+            } else if (newValue < curDomStyle.min) { // curDomStyle.min
+              this._width = curDomStyle.min
+            } else {
+              this._width = newValue
+            }
+          },
+          get() {
+            return this._width
+          }
+        })
+        // 设置拖动
+        let isResizing = false
+        let startWidth = 0
+        let startX = 0
+        const move = (e) => {
+          if (isResizing) {
+            const newWidth = startWidth + (e.clientX - startX)
+            curDomStyle.width = newWidth
+            el.style.width = curDomStyle.width + 'px'
+          }
+        }
+        dargNodeDom.addEventListener('mousedown', (e) => {
+          startWidth = el.offsetWidth
+          startX = e.clientX
+          isResizing = true
+          document.addEventListener('mousemove', move)
+        })
+        document.addEventListener('mouseup', () => {
+          isResizing = false
+          document.removeEventListener('mousemove', move)
+        })
+      }
+    })
   }
 }
