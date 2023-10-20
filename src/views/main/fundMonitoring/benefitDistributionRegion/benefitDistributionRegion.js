fbb6c096b829de06d06873580a3b4155b9fe4c67
hupengcheng <1286335855@qq.com>
2023-10-20 17:49:17 +0800

    fix:requestUrl文件提交

--- src/views/main/fundMonitoring/benefitDistributionRegion/benefitDistributionRegion.js
+++ src/views/main/fundMonitoring/benefitDistributionRegion/benefitDistributionRegion.js
@@ -2,7 +2,7 @@
  * @Author: hupengcheng 1286335855@qq.com
  * @Date: 2023-10-19 11:33:16
  * @LastEditors: hupengcheng 1286335855@qq.com
- * @LastEditTime: 2023-10-20 16:54:45
+ * @LastEditTime: 2023-10-20 17:37:35
  * @FilePath: \platformweb-fb\src\views\main\fundMonitoring\benefitDistributionRegion\benefitDistributionRegion.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
