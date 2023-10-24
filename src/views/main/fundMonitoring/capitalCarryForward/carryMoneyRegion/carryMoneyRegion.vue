a1c02f754915df19a7f869626f233c1fd923c3ac
金江华 <924318103@qq.com>
2023-10-24 13:44:29 +0800

    fix： 陕西_结转资金支出_分地区_默认reportCode传参

--- src/views/main/fundMonitoring/capitalCarryForward/carryMoneyRegion/carryMoneyRegion.vue
+++ src/views/main/fundMonitoring/capitalCarryForward/carryMoneyRegion/carryMoneyRegion.vue
@@ -341,7 +341,7 @@ export default {
     queryTableDatas(isFlush = true) {
       const param = {
         isFlush,
-        reportCode: this.params5 || 'CarryMoneyRegion'
+        reportCode: this.params5.reportCode || 'zdjzzjfdqjzqkb'
       }
       this.tableLoading = true
       HttpModule.queryTableDatas(param).then((res) => {
