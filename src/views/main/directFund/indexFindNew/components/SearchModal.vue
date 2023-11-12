6ec2fe43dca6fb374f5b6e2fad7ba174ba016c3e
fengchongyang <fengchongyang@bosssoft.com.cn>
2023-11-12 14:37:34 +0800

    fix: 监控查看项目详情异常修改(调整获取group位置)

--- src/views/main/directFund/indexFindNew/components/SearchModal.vue
+++ src/views/main/directFund/indexFindNew/components/SearchModal.vue
@@ -390,11 +390,11 @@ export default {
         this.mofDivCode = obj.row?.mofDivCode || ''
         this.fiscalYear = obj.row?.fiscalYear || ''
         let groupUrl = ''
-        let groups = window.gloableToolFn.serverGatewayMap.getGroups || {}
+        let groups = window.gloableToolFn.getGroups || {}
         Object.keys(groups).forEach(key => {
           console.log(key, '-------------')
           if (groups[key].indexOf(this.mofDivCode?.slice(0, 4)) !== -1) {
-            groupUrl = window.gloableToolFn.serverGatewayMap.getProjectUrl[key]
+            groupUrl = window.gloableToolFn.getProjectUrl[key]
             let url = `${groupUrl}/ProjectDetailIframe?isShowHead=0&tokenid=${this.$store.getters.getLoginAuthentication.tokenid}&appguid=ystztj&proGuid=${this.proGuid}&mofDivCode=${this.mofDivCode}&fiscalYear=${this.fiscalYear}`
             console.log('访问地址：', url)
             window.open(url)
