06fceeb116b655a0119c5b715294bd793474d2cd
盛强 <18811740889@163.com>
2023-11-16 09:39:41 +0800

    fix【厦门问题】惠企利民支付明细导入样式修改

--- src/views/main/fundMonitoring/benefitPeople/project/benefitPeople.js
+++ src/views/main/fundMonitoring/benefitPeople/project/benefitPeople.js
@@ -78,6 +78,7 @@ export let proconf = {
       itemRender: {
         name: '$vxeInput',
         props: {
+          clearable: true,
           placeholder: '项目名称'
         }
       }
@@ -100,6 +101,7 @@ export let proconf = {
               children: 'children'
             },
             placeholder: '预算单位',
+            clearable: true,
             multiple: true,
             readonly: true,
             isleaf: true
@@ -117,6 +119,7 @@ export let proconf = {
       itemRender: {
         name: '$vxeInput',
         props: {
+          clearable: true,
           placeholder: '指标文号'
         }
       }
@@ -131,6 +134,7 @@ export let proconf = {
       itemRender: {
         name: '$vxeInput',
         props: {
+          clearable: true,
           placeholder: '资金用途'
         }
       }
