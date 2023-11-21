1bc3c17a3edfb19ea45e43f77957994ed7baeab9
王璐 <wanglu-zhixin@bosssoft.com.cn>
2023-11-21 15:40:16 +0800

    -- fix: RH-2165

--- src/views/main/MointoringMatters/SystemLevelRules/children/addDialog.vue
+++ src/views/main/MointoringMatters/SystemLevelRules/children/addDialog.vue
@@ -1348,7 +1348,7 @@ export default {
       let regulationType = this.$store.state.curNavModule.f_FullName?.substring(0, 3)
       if (regulationType === '部门级') {
         param.elementCode = 'DEPARTMENT'
-        if(this.$store.getters.isSx){
+        if(this.$store.getters.isSx||this.$store.getters.isFuJian){
           param.elementCode = 'AGENCY'
         }
         param.wheresql = 'and code like \'' + this.$store.state.userInfo.orgcode + '%\''
