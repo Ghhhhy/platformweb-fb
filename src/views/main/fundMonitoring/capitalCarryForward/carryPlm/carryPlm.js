6f3c2d23258773b28bc0af132819a81dbcc006fb
shaohengpeng <2973359386@qq.com>
2023-12-26 17:44:28 +0800

    fix(直达机制资金分地区结转情况表&直达机制项目全生命周期管理情况表)-要加一个资金的搜索条件，且选择后传参要能传的进SQL语句，搜索生效

--- src/views/main/fundMonitoring/capitalCarryForward/carryPlm/carryPlm.js
+++ src/views/main/fundMonitoring/capitalCarryForward/carryPlm/carryPlm.js
@@ -43,10 +43,38 @@ const proconf = {
           placeholder: '业务年度'
         }
       }
+    },
+    {
+      title: '资金名称',
+      field: 'proCodes',
+      width: '8',
+      align: 'left',
+      name: '$vxeTree',
+      itemRender: {
+        name: '$vxeTree',
+        options: [],
+        props: {
+          config: {
+            valueKeys: ['code', 'name', 'id'],
+            format: '{name}',
+            treeProps: {
+              labelFormat: '{code}-{name}', // {code}-{name}
+              nodeKey: 'id',
+              label: 'label',
+              children: 'children'
+            },
+            placeholder: '资金名称',
+            multiple: true,
+            readonly: false,
+            isleaf: true
+          }
+        }
+      }
     }
   ],
   highQueryData: {
-    fiscalYear: ''
+    fiscalYear: '',
+    proCodes: ''
   },
   basicInfo: {
     type: 'form',
