8c260fddc1ebd94999fa3cedd1eff17186133b32
西安-郭宇龙 <2194367529@qq.com>
2023-12-29 19:31:19 +0800

    月报录入作废查询

--- src/views/main/MonthlyInfoTbl/MonthlyInfoTbl.vue
+++ src/views/main/MonthlyInfoTbl/MonthlyInfoTbl.vue
@@ -1802,6 +1802,11 @@ export default {
           qid: '315DDFED8D444D678B5C65E6DB5087E3'
         },
         {
+          code: '5',
+          tid: 'C9FBBBAB7C254172AD54674B0BC75BD3',
+          qid: '315DDFED8D444D678B5C65E6DB5087E3'
+        },
+        {
           code: '0',
           tid: 'C9FBBBAB7C254172AD54674B0BC75BD3',
           qid: '315DDFED8D444D678B5C65E6DB5087E3'
@@ -1815,7 +1820,7 @@ export default {
       hideZero: true,
       tableCountUrl: '/dfr-monitor-service/dfr/pmProjectInfoDetMon/count',
       tableCountParams: {
-        statusCodeArr: ['1', '2', '9'],
+        statusCodeArr: ['1', '2', '5', '9'],
         appId: 'pm_project_info_det_month'
       },
       toolBarStatusBtnConfig: {
@@ -1847,6 +1852,15 @@ export default {
             label: '已办事项',
             code: '2',
             curValue: '2'
+          },
+          {
+            type: 'button',
+            iconName: 'base-yiban.png',
+            iconNameActive: 'base-yiban-active.png',
+            iconUrl: '',
+            label: '已作废',
+            code: '5',
+            curValue: '5'
           }
         ],
         curButton: {
